c62cb4b51c7217e1842ddd00b9d5ee5efefb36d6
TheFrostlixen <mgfredrickson@nevada.unr.edu>
2015-05-08 21:46:02 -0700

    tool initial commit

---
+++ MinecraftUtil/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace MinecraftUtil
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Designer variable used to keep track of non-visual components.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Timer timer1;
+
+        /// <summary>
+        /// Disposes resources used by the form.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (components != null)
+                {
+                    components.Dispose();
+                }
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// This method is required for Windows Forms designer support.
+        /// Do not change the method contents inside the source code editor. The Forms designer might
+        /// not be able to load this method if it was changed manually.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.labelMotd = new System.Windows.Forms.Label();
+            this.groupPlayers = new System.Windows.Forms.GroupBox();
+            this.labelPlayers = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupPlayers.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1Tick);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(0, -1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(184, 394);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.labelMotd);
+            this.tabPage1.Controls.Add(this.groupPlayers);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.numericUpDown1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(176, 368);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Players";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // labelMotd
+            // 
+            this.labelMotd.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelMotd.Location = new System.Drawing.Point(6, 55);
+            this.labelMotd.Name = "labelMotd";
+            this.labelMotd.Size = new System.Drawing.Size(164, 23);
+            this.labelMotd.TabIndex = 11;
+            // 
+            // groupPlayers
+            // 
+            this.groupPlayers.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupPlayers.Controls.Add(this.labelPlayers);
+            this.groupPlayers.Location = new System.Drawing.Point(6, 81);
+            this.groupPlayers.Name = "groupPlayers";
+            this.groupPlayers.Size = new System.Drawing.Size(164, 281);
+            this.groupPlayers.TabIndex = 10;
+            this.groupPlayers.TabStop = false;
+            this.groupPlayers.Text = "Players Online: ";
+            // 
+            // labelPlayers
+            // 
+            this.labelPlayers.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelPlayers.Location = new System.Drawing.Point(6, 19);
+            this.labelPlayers.Name = "labelPlayers";
+            this.labelPlayers.Size = new System.Drawing.Size(152, 259);
+            this.labelPlayers.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(35, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Port:";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Server IP:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(72, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(98, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "23.95.29.207";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Location = new System.Drawing.Point(72, 32);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(98, 20);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            25565,
+            0,
+            0,
+            0});
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.panel1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(176, 368);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Coordinates";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(176, 368);
+            this.panel1.TabIndex = 0;
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Image = global::MinecraftUtil.Properties.Resources.glassarcadia;
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(104, 17);
+            this.toolStripStatusLabel2.Text = "Not Connected";
+            this.toolStripStatusLabel2.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.toolStripStatusLabel2.Click += new System.EventHandler(this.toolStripStatusLabel2_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 396);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(184, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(176, 368);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Resources";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(184, 418);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.statusStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Minecraft Utility";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1FormClosing);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.groupPlayers.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label labelMotd;
+        private System.Windows.Forms.GroupBox groupPlayers;
+        private System.Windows.Forms.Label labelPlayers;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabPage tabPage3;
+    }
+}
---
+++ MinecraftUtil/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.Web.Script.Serialization;
+using System.Collections.Generic;
+using System.Collections;
+using System.IO;
+
+/**
+ * ADDITIONAL FEATURES:
+ *  Circle/Golden Rectangle Generator
+ *  Food guide
+ *  Brewing guide
+ *  Customizable Redstone Circuitry Dictionary
+ *  Coordinate Tracker
+**/ 
+
+
+namespace MinecraftUtil
+{
+    public partial class Form1 : Form
+    {
+        private const string _QUERY = "http://api.minetools.eu/query/{0}/{1}";
+        private const string _PING = "http://api.minetools.eu/ping/{0}/{1}";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (!File.Exists( "MinecraftUtil.exe.config" ))
+            {
+                CreateConfigFile();
+            }
+
+            // write settings data
+            textBox1.Text = Properties.Settings.Default.ServerAddress;
+            numericUpDown1.Value = Properties.Settings.Default.ServerPort;
+
+            // start timer for fetching data
+            timer1.Start();
+            RefreshData();
+        }
+
+        private void RefreshData()
+        {
+            try
+            {
+                // get the query strings
+                string queryString  = new WebClient().DownloadString( string.Format( _QUERY, textBox1.Text, numericUpDown1.Value.ToString() ) );
+                //string pingString   = new WebClient().DownloadString( string.Format( _PING,  textBox1.Text, numericUpDown1.Value.ToString() ) );
+
+                // de-serialize into a dictionary
+                var ser = new JavaScriptSerializer();
+                Dictionary<string,object> dict = ser.Deserialize<Dictionary<string, object>>( queryString );
+
+                // populate data fields from dict
+                groupPlayers.Text = "Players Online: " + dict["Players"].ToString();
+                labelMotd.Text = dict["HostName"].ToString();
+                ListPlayers( dict["Playerlist"] );
+
+                // finally, if nothing has gone wrong, show 'connected' status
+                toolStripStatusLabel2.Text = "Connected";
+            }
+            catch (WebException e)          { ResetData(); }
+            catch (KeyNotFoundException e)  {  }
+            catch (ArgumentException e)     { ResetData(); }
+        }
+
+        private void ResetData()
+        {
+            toolStripStatusLabel2.Text = "Not connected";
+            labelPlayers.Text = "";
+            labelMotd.Text = "";
+            groupPlayers.Text = "Players Online: ";
+        }
+
+        private void ListPlayers( object list )
+        {
+            // build the list of online players
+            if ( list.ToString() == "null" )
+            {
+                labelPlayers.Text = "No players are online";
+            }
+            else
+            {
+                ArrayList array = (ArrayList)list;
+
+                // play with the label text
+                labelPlayers.Text = "";
+                foreach (string s in array)
+                {
+                    // append online players
+                    labelPlayers.Text += s + "\r\n";
+                }
+            }
+        }
+
+        void Timer1Tick(object sender, EventArgs e)
+        {
+            // every 5000ms refresh the data
+            RefreshData();
+        }
+
+        void Form1FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // save ip and port data
+            Properties.Settings.Default.ServerAddress = textBox1.Text;
+            Properties.Settings.Default.ServerPort = numericUpDown1.Value;
+            Properties.Settings.Default.Save();
+        }
+
+        private void toolStripStatusLabel2_Click(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void CreateConfigFile()
+        {
+            // this is disgusting and i'm ashamed of myself, go away
+            File.WriteAllText( "MinecraftUtil.exe.config", "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\r\n<configuration>\r\n\t<configSections>\r\n\t\t<sectionGroup name=\"userSettings\" type=\"System.Configuration.UserSettingsGroup, System, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089\" >\r\n\t\t\t<section name=\"MinecraftUtil.Properties.Settings\" type=\"System.Configuration.ClientSettingsSection, System, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089\" allowExeDefinition=\"MachineToLocalUser\" requirePermission=\"false\" />\r\n\t\t</sectionGroup>\r\n\t</configSections>\r\n\t<userSettings>\r\n\t\t<MinecraftUtil.Properties.Settings>\r\n\t\t\t<setting name=\"ServerAddress\" serializeAs=\"String\">\r\n\t\t\t\t<value />\r\n\t\t\t</setting>\r\n\t\t\t<setting name=\"ServerPort\" serializeAs=\"String\">\r\n\t\t\t\t<value>0</value>\r\n\t\t\t</setting>\r\n\t\t</MinecraftUtil.Properties.Settings>\r\n\t</userSettings>\r\n</configuration>" );
+        }
+    }
+}
