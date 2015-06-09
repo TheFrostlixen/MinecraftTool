34f99a5e073c5880afd517b72d02451d100d952d
TheFrostlixen <matthewgfredrickson@gmail.com>
2015-06-09 09:54:06 -0700

    did more stuff to web, still not working

--- web/js/serverping.js
+++ web/js/serverping.js
@@ -1,23 +1,35 @@
+var json;
+
 function ping() {
 	// build url
-	var url = "http://api.minetools.eu/query/".concat( document.getElementById("server").value, "/", document.getElementById("port").value );
+	var server = encodeURIComponent( document.getElementById("server").value );
+	var port = encodeURIComponent( document.getElementById("port").value );
+	var url = "http://api.minetools.eu/query/".concat( server, "/", port );
 	
 	// update status
 	document.getElementById("url").innerHTML = url;
 
-	// jQuery is not defined, idk why, fml
-	if (jQuery){
-		jQuery.get( url+"?jsoncallback=?", function( data )
-		{
-			$("body")
-			.append("Name: " + data.Map)
-			.append("Time: " + data.Players);
-		}, "json" );
-	}
-	else{ alert("jQuery is undefined."); }
+	// get json data from server
+	var httpxml = new XMLHttpRequest({mozSystem: true});
+	httpxml.onload = setJson;
+	/* So the problem is that the "Access-Control-Allow-Origin" header (CORS) is not enabled. This is a server-side issue, need a workaround. FML. */
+	httpxml.onerror = function() { alert('Error communicating with server.'); };
+	httpxml.open( "GET", url, false );
+	httpxml.send();
+	//json = httpxml.responseText;
+	//var obj = JSON.parse( json );
+	//document.getElementById("p1").innerHTML = obj.Playerlist;
 	
-	// parse json data into list
+	// display playerlist on html side
 	
-	// display list on html side
-	
+}
+
+function getJson(data) {
+	alert('should not hit this');
+	json = data;
+	document.getElementById("p2").innerHTML = json + "++";
+}
+
+function setJson() {
+	document.getElementById("p1").innerHTML = this.responseText + "--";
 }
